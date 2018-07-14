5a2c697d218f9c09fce5594c01d0e9b9221957a2
Segovfrank <segovfrank@gmail.com>
2018-07-14 12:57:14 +0900

    Merge CRUD with user system

--- routes/index.js
+++ routes/index.js
@@ -36,5 +36,48 @@ router.get('/user/profile', function(req, res, next){
   res.render('user/profile');
 });
 
+router.get('/dashboard',function(req,res,next){
+               res.render('shop/dashboard');
+
+           });
+
+router.post('/insert',function(req,res,next){
+                var product = {
+                    imagePath: req.body.productImg,
+                    title: req.body.productName,
+                    description: req.body.productDescription,
+                    price: req.body.productPrice
+                };
+
+                mongo.connect(url,function(err,db){
+                    assert.equal(null,err);
+                    db.collection('products').insert(product, function(err,result){ // callback function too
+                        assert.equal(null, err);
+                        console.log("Product inserted...");
+                        db.close();
+                    });
+                });
+
+                Product.find(function(err, docs){
+                  var productChucks = [];
+                  var chunkSize = 3;
+                  for (var i = 0; i < docs.length; i += chunkSize) {
+                    productChucks.push(docs.slice(i, i + chunkSize));
+                  }
+                  res.render('shop/index', { title: 'MongoDB Shopping Cart', products: productChucks });
+
+                });
+              });
+
+
+
+router.post('/update',function(req,res,next){
+
+            });
+
+router.post('/delete',function(req,res,next){
+
+            });
+
 
 module.exports = router;
